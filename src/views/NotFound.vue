d2dc7b3975ff9747385f05f73107ed5369d22f0d
Azizul Islam Rahad <65822873+gazirahad7@users.noreply.github.com>
2023-09-17 11:48:26 +0600

    Added

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>404, Not Found Page !</h1>
+  <div class="d-flex justify-content-center align-items-center min-vh-100">
+    <div class="text-center">
+      <h1>404, Not Found Page 🥲</h1>
+      <button onclick="history.back()" class="btn btn-warning my-5">
+        Go Back
+      </button>
+    </div>
+  </div>
 </template>
