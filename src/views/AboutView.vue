c4569c46ad726436f736032bb0dfa20504a0801e
Azizul Islam Rahad <65822873+gazirahad7@users.noreply.github.com>
2023-09-17 11:38:06 +0600

    Added

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class="about">
+  <div class="about my-5">
     <h1>This is an about page</h1>
+
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus,
+      debitis! Odio accusantium dignissimos amet placeat sapiente, ipsum, eaque
+      repellendus magnam excepturi modi ratione labore in cum voluptatibus quae
+      accusamus esse? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Atque, velit aliquid eius eveniet nobis animi rerum autem cupiditate
+      accusamus natus suscipit omnis consequuntur voluptate repellat voluptatem
+      incidunt eligendi totam veritatis. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Itaque nobis beatae, nam ipsam tempora cum quos odit ut
+      temporibus natus nulla, nemo saepe animi necessitatibus ipsa ea adipisci
+      nisi quis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+      cumque odio laudantium nemo ducimus laboriosam ex sunt rem excepturi in ab
+      maxime amet, placeat ipsa aut saepe! Itaque, officia facilis! Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Cum perspiciatis sint eaque
+      ipsum cupiditate? Fuga illo, rerum corrupti repudiandae repellat quos
+      officiis vero odit accusantium, corporis perspiciatis aspernatur quaerat
+      veritatis.
+    </p>
   </div>
 </template>
 
